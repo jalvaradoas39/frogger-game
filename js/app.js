03b51ab1b6c308cab85a8261825228cbb39a44fb
Jorge Alvarado <jalvaradoas39@outlook.com>
2018-09-04 18:56:04 -0400

    Add Enemy constructor

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// storage for enemy objects
+let allEnemies = [];
+
+// ****** ENEMY ******
+
+// constructor
+let Enemy = function() {
+	this.ctx = window.ctx;
+	this.sprite = 'images/enemy-bug.png';
+
+};
+
+Enemy.prototype.update = function(dt) {
+
+};
+
+Enemy.prototype.render = function() {
+	// ctx.drawImage();
+};
+
+
+
+
+
+
