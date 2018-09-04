6dd0fbf5a508e2d819ddfc43a311391b98075144
Jorge Alvarado <jalvaradoas39@outlook.com>
2018-09-04 14:23:29 -0400

    Add image loading utility
    - public API methods created
    - load() checks if string or array provided in argument to take appropriate action
    - _load() creates, reuses and cache's images
    - isReady() checks if all images loaded
    - onReady() pushes callback functions onto stack
    - get() retrieves cached imageElem

--- js/resources.js
+++ js/resources.js
@@ -1,20 +1,77 @@
 (function() {
 
 	let resourceCache = {};
+	let readyCallbacks = [];
+	let doc = window.document;
+
 
 
 	function load(urlOrArr) {
-		if (urlOrArr instanceOf Array) {
-			console.log('an array');
+		// checks if developer passed an array of urls else assume just one
+		if (urlOrArr instanceof Array) {
+			urlOrArr.forEach(function(url) {
+				_load(url);
+			});
 		} else {
-			console.log('a string');
+			_load(urlOrArr);
 		}
+	}
+
+
+	function _load(url) {
+		if (resourceCache[url]) {
+			return resourceCache[url];
+
+		} else {
+			let imageElem = doc.createElement('img');
+			imageElem.src = url;
+
+			// attach event listener to each image
+			imageElem.onload = function(evt) {
+					// add image to cache
+					// ex. resourceCache{'images/stone-block.png' : <img src="images/stone-block.png"/> , ...}
+					resourceCache[url] = imageElem;
+
+					if (isReady()) {
+						// console.log('it is ready');
+						readyCallbacks.forEach(function(func) {
+							func();
+						});
+					}
+			}
+
+			resourceCache[url] = false;
+		}
+	}
+
+
+	function onReady(func) {
+		readyCallbacks.push(func);
+	}
+
+	function isReady() {
+		// assume already loaded
+		let ready = true;
+
+		for (let key in resourceCache) {
+			if ( resourceCache.hasOwnProperty(key) && !resourceCache[key] ) {
+				ready = false;
+			}
+		}
+		return ready;
+	}
+
+	function get(url) {
+		return resourceCache[url];
 	}
 
 
 	// public api
 	window.Resources = {
-		load: load
+		load: load,
+		onReady: onReady,
+		isReady: isReady,
+		get: get
 	};
 
 })();
@@ -23,81 +80,6 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// (function() {
-
-// 	// test data
-// 	let domOutput = document.getElementById('test');
-
-// 	function preloadImages(array) {
-// 		if (!preloadImages.list) {
-// 			preloadImages.list = [];
-// 		}
-
-// 		let list = preloadImages.list;
-
-
-// 		for (let i=0; i < array.length; i++) {
-// 			let imgElem = document.createElement('img');
-// 			imgElem.alt = 'player';
-// 			imgElem.src = 'images/' + array[i];
-// 			imgElem.classList.add('puffImgSize')
-
-// 			imgElem.onload = function(evt) {
-				
-// 				// if img already cached in array, get it's index; otherwise -1 is returned for non-existent
-// 				let index = list.indexOf(evt.currentTarget);
-// 				// exec if image elem currently in list
-// 				if (index !== -1) {
-// 					list.splice(index, 1);
-// 				}
-
-// 				domOutput.appendChild(imgElem);
-// 				list.push(imgElem);
-// 			}
-// 		}
-// 		console.log(list);
-
-// 	}
-
-
-// 	let val = preloadImages(['char-boy.png', 'char-pink-girl.png', 'enemy-bug.png', 'Rock.png']);
-
-// })();
 
 
 
